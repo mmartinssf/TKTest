0df7ca0c700004a7c15ef38d946e14c9ef30bfa9
Michael Martin <mmartin@softstackfactory.org>
2016-10-31 20:33:02 +0000

    Charts Installed

---
+++ www/js/app.js
@@ -0,0 +1,80 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+/*global angular*/
+/*global cordova*/
+angular.module('starter', ['ionic', 'TKTestQuestions', 'starter.controllers', 'TKTestAnswers', 'chart.js', 'TKResultsButton', 'RESTServices'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if (window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/');
+  $stateProvider
+    .state('lobby', {
+      url: '/',
+      templateUrl: 'templates/lobby.html',
+      controller: 'LobbyCtrl',
+    })
+
+  .state('question', {
+    url: '/question:questionID',
+    templateUrl: 'templates/question.html',
+    controller: 'QuestionsCtrl',
+    resolve: {
+      testInfo: function($stateParams, TKTestQuestionService) {
+        return TKTestQuestionService.getQuestion($stateParams.questionID);
+      }
+    }
+  })
+
+  .state('history', {
+    url: '/history',
+    templateUrl: 'templates/history.html',
+    controller: 'HistoryCtrl',
+    resolve: {
+      tests: ['TKAnswersService', function(TKAnswersService) {
+        return TKAnswersService.getTests();
+      }]
+    }
+  })
+
+  .state('results', {
+    url: '/results',
+    templateUrl: 'templates/results.html',
+    controller: 'ResultsCtrl'
+  });
+  
+  .state('landing', {
+    
+    
+  });
+  
+  .state('register', {
+    
+    
+  });
+  
+  .state('login', {
+    
+    
+  });
+
+});
---
+++ www/js/controllers/resultsCtrl.js
@@ -0,0 +1,53 @@
+/*global angular*/
+angular.module('starter.controllers')
+    .controller('ResultsCtrl', ['$scope', 'TKAnswersService', 'TKResultsButtonService', '$ionicHistory', '$state',
+        function($scope, TKAnswersService, TKResultsButtonService, $ionicHistory, $state) {
+
+            $scope.menuButtonTapped = function() {
+                $ionicHistory.nextViewOptions({
+                    historyRoot: true,
+                    disableBack: true
+                });
+                $state.go('lobby');
+            };
+
+            $scope.labels = ["Competing", "Collaborating", "Compromising", "Avoiding", "Accommodating"];
+
+            var answersInfo = TKAnswersService.getAnswers();
+
+            function returnPercentage(value) {
+                return (value / 12) * 100;
+            }
+
+            $scope.data = [
+                [returnPercentage(answersInfo["competing"]), returnPercentage(answersInfo["collaborating"]),
+                    returnPercentage(answersInfo["compromising"]), returnPercentage(answersInfo["avoiding"]), returnPercentage(answersInfo["accommodating"])
+                ]
+            ];
+
+            $scope.shouldShowButton = TKResultsButtonService.getShouldShowMenuButton();
+
+            $scope.options = {
+                scaleIntegersOnly: true,
+                animation: false,
+                responsive: true,
+                maintainAspectRatio: false,
+                scaleOverride: true,
+                scaleSteps: 4,
+                scaleStepWidth: 25,
+                scaleStartValue: 0,
+                scaleLabel: "<%=value%>" + "%",
+                tooltipTemplate: "<%if (label){%><%=label%>: <%}%><%= value.toFixed(0) %>" + "%",
+            };
+
+            $scope.colours = [{
+                fillColor: "rgba(151,187,205,0.2)",
+                strokeColor: "rgba(15,187,25,1)",
+                pointColor: "rgba(15,187,25,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(151,187,205,0.8)"
+            }]
+        }
+
+    ]);
